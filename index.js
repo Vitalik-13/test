a1bf3a88bb3487a246f6d302baaa7fc954e3eda1
vitalikkk <vitalikgermak6@gmail.com>
2023-11-29 12:01:34 +0200

    change web-site content

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+$(document).ready(function () {
+  $(".rewies-slider").slick({
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 488,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
+
 const fixedHeader = document.querySelector(".header");
 const showButton = document.querySelector(".button__header");
 const changeColorLogo = document.querySelector(".logo-span");
@@ -6,11 +30,8 @@ document.addEventListener("scroll", () => {
   if (scrollY > 50) {
     fixedHeader.classList.add("fixed_header");
     showButton.classList.add("show");
-    changeColorLogo.classList.add("color");
   } else {
     fixedHeader.classList.remove("fixed_header");
-    showButton.classList.remove("show");
-    changeColorLogo.classList.remove("color");
   }
 });
 
@@ -21,6 +42,24 @@ const imageTitleDescripshion = document.querySelectorAll(".shewron");
 descripshionListTitle.forEach((item, idx) => {
   item.addEventListener("click", (e) => {
     showSpanDescripshion[idx].classList.toggle("show");
+    descripshionListTitle[idx].classList.toggle("change-color");
     imageTitleDescripshion[idx].classList.toggle("arrow-rotation");
+  });
+});
+
+const burgerMenu = document.querySelector("#check");
+const menuNavigation = document.querySelector(".menu-navigation");
+burgerMenu.addEventListener("change", () => {
+  if (burgerMenu.checked) {
+    menuNavigation.classList.add("transform-menu");
+  } else {
+    menuNavigation.classList.remove("transform-menu");
+  }
+});
+const listNavigation = document.querySelectorAll(".list-navigation");
+listNavigation.forEach((item) => {
+  item.addEventListener("click", () => {
+    menuNavigation.classList.remove("transform-menu");
+    burgerMenu.checked = false;
   });
 });
